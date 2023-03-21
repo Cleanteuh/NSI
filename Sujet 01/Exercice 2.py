b8a8e2c4f159d7b2e44603d9e214396584de28a4
Nicolas Becharat <nicolas.becharat@gmail.com>
2023-03-21 22:13:18 +0100

    Initial commit

---
+++ Sujet 01/Exercice 2.py
@@ -0,0 +1,20 @@
+urne = ['A', 'A', 'A','B', 'C', 'B', 'C','B', 'C', 'B']
+
+def depouille(urne):
+    resultat = {"A" : 0, "B" : 0, "C": 0}
+    for bulletin in urne:
+        if bulletin in resultat:
+            resultat[bulletin] = resultat[bulletin] + 1
+        else:
+            resultat[bulletin] = 1
+    return resultat
+
+def vainqueur(election):
+    vainqueur = ''
+    nmax = 0
+    for candidat in election:
+        if election[candidat] > nmax :
+            nmax = election[candidat] 
+            vainqueur = candidat
+    liste_finale = [nom for nom in election if election[nom] == nmax]
+    return liste_finale
